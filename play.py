791041d691b44a2e6a3a1ba71953021b37beaf1f
zyinshi <zyinshi@gmail.com>
2014-11-03 10:51:37 -0800

    small modification on I/O

--- play.py
+++ play.py
@@ -1,12 +1,11 @@
 from sudoku import ValidationException, Solution
 import csv
 import string
-
-file = open("board1.csv","rb")
+filename = raw_input('Enter the filename of your unsolved sudoku puzzle: ') or 'board1.csv'
+raw_name = filename.split('.')[0];
+file = open(filename,"rb")
 inp = []
 for line in file.xreadlines():
-	# print line
-	# inp.append(string.split(line,','))
 	line = [i.strip() for i in line.split(',')]
 	inp.append(line)
 
@@ -20,6 +19,6 @@ sl.solveSudoku(board)
 for rows in board:
 	print rows 
 
-resultFile = open("solved.csv",'wb')
+resultFile = open(raw_name + "_solved.csv",'wb')
 wr = csv.writer(resultFile, dialect='excel')
 wr.writerows(board)
